6c7b1408bc5cca4cb62d438d7a6cbcdaa2c19ec6
Aidan Valva <aidan.valva@student.allenisd.org>
2022-09-16 15:07:31 -0500

    trying new alorithm

--- main.py
+++ main.py
@@ -76,33 +76,34 @@ arr = b.split()
 #     else:  # to handle the end part of recursion if there is only one element in the erray then this just returns the array
 #         return array
 
-def sort4(arr, n):
-    midpoint = n // 2 # grabs the midpoint of the arr
-    while midpoint > 0:
-#loops until no more
-        for i in range(midpoint, n):
-            tmp = arr[i] # allows for var i to hold a dfferent values while saving the other in var tmp
-            j = i # checks for when var i is in correct arr value
-            while j >= midpoint and arr[j - midpoint] > tmp: # allows for j to work as a variable within the sort arr, and protects it for going into negative values
-                arr[j] = arr[j - midpoint]
-                j -= midpoint
-                #puts arr values in place due to //= 2, returns original i value in the array from tmp value
-            arr[j] = tmp 
-        midpoint //= 2
+# def sort4(arr, n):
+#     midpoint = n // 2 # grabs the midpoint of the arr
+#     while midpoint > 0:
+# #loops until no more
+#         for i in range(midpoint, n):
+#             tmp = arr[i] # allows for var i to hold a dfferent values while saving the other in var tmp
+#             j = i # checks for when var i is in correct arr value
+#             while j >= midpoint and arr[j - midpoint] > tmp: # allows for j to work as a variable within the sort arr, and protects it for going into negative values
+#                 arr[j] = arr[j - midpoint]
+#                 j -= midpoint
+#                 #puts arr values in place due to //= 2, returns original i value in the array from tmp value
+#             arr[j] = tmp 
+#         midpoint //= 2
        
-size = len(arr)
-sort4(arr, size)
+# size = len(arr)
+# sort4(arr, size)
 #returns outputs to the size of the arr,
 
-# def sort5(arr):
-#     for i in range(1, len(arr)):
-#         tmp = arr[i]
-#         j = i
+def sort5(arr):
+    for i in range(1, len(arr)): #starts from the first, "sorted", variable of the arr, while the for loop takes care of reusing the while loop until properly sorted
+        tmp = arr[i]
+        j = i
 
-#         while j > 0 and arr[j - 1] > tmp: while there are still values to sort it will always hold a integer high than 0, if not it stops recursion
-#             arr[j] = arr[j - 1]
-#             j = j - 1
-#             arr[j] = tmp
-# sort5(arr)
+        while j > 0 and arr[j - 1] > tmp: #this allows the sort func to push the alphabetically descending lesser var to the right, or down while keeping the higher in place, while amount of needed sorts is above 0
+            arr[j] = arr[j - 1]
+            j = j - 1
+            arr[j] = tmp
+sort5(arr)
+
 print(" ")
 print(*arr,sep="\n")
